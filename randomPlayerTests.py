93e0288571014781b6bee9ab2c8a9a9da9115f11
jjmcc1 <165006970+jjmcc1@users.noreply.github.com>
2024-03-26 09:22:50 +0000

    Update randomPlayerTests.py

--- randomPlayerTests.py
+++ randomPlayerTests.py
@@ -1,9 +1,1 @@
-from constants import ROWS, COLUMNS
 
-def test_get_random_move(self):
-    move = self.game.get_random_move()
-    self.assertIsInstance(move, tuple)
-    self.assertGreaterEqual(move[0], 0)
-    self.assertLess(move[0], ROWS)
-    self.assertGreaterEqual(move[1], 0)
-    self.assertLess(move[1], COLUMNS)
